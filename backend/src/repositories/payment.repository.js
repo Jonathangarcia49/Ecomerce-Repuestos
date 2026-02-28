a881437604efc257c1b7268b4ca969923f2c54db
Jonathangarcia49 <jonathan062002garcia@hotmail.com>
2026-02-28 01:13:55 -0500

    feat: mejorar la lógica de creación y obtención de órdenes con transacciones y ordenamiento por fecha

--- backend/src/repositories/payment.repository.js
+++ backend/src/repositories/payment.repository.js
@@ -1,6 +1,12 @@
-
 import { Order } from '../models/Order.js';
+import { User } from '../models/User.js';
 
-export const createOrder = (data) => Order.create(data);
+export const createOrder = (data, transaction) =>
+  Order.create(data, { transaction });
+
 export const getOrdersByUser = (userId) =>
-  Order.findAll({ where: { UserId: userId }, order: [['id', 'DESC']] });
+  Order.findAll({
+    where: { UserId: userId },
+    order: [['createdAt', 'DESC']],
+  });
+
